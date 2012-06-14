262c90e09e4e0a4b174cba60e35b871d6a2e9324
Bastian Eicher <bastian@eicher.net>
2012-06-14 17:43:58 +0200

    Improved archive build script to only add selected parts from "data"

--- setup/build.cmd
+++ setup/build.cmd
@@ -21,12 +21,17 @@ echo Building SDK archive...
 if not exist "%TargetDir%" mkdir "%TargetDir%"
 if exist "%TargetDir%\omegaengine.zip" del "%TargetDir%\omegaengine.zip"
 cd /d "%~dp0..\build\ReleaseSDK"
+if exist Base rmdir /s /q Base
+mkdir Base\Graphics\Shaders
+copy ..\data\Graphics\Shaders Base\Graphics\Shaders\ > NUL
+mkdir Base\Meshes\Engine
+copy ..\data\Meshes\Engine Base\Meshes\Engine\ > NUL
+mkdir Base\Textures\Water
+copy ..\data\Textures\flag.png Base\Textures\flag.png > NUL
+copy ..\data\Textures\Water Base\Textures\Water\ > NUL
+mkdir Base\GUI\Textures
+copy ..\data\GUI\Textures Base\GUI\Textures\ > NUL
 zip -9 -r "%TargetDir%\omegaengine.zip" . > NUL
-if errorlevel 1 pause
-cd /d "%~dp0.."
-mv data Base
-zip -9 -r "%TargetDir%\omegaengine.zip" Base > NUL
-mv Base data
 if errorlevel 1 pause
 cd /d "%~dp0"
 
