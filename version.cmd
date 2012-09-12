769f5dd686c24434139c9131ea687e2ebccc2854
Bastian Eicher <bastian@eicher.net>
2012-09-12 18:54:39 +0200

    Simplified version script

--- version.cmd
+++ version.cmd
@@ -5,4 +5,3 @@ set /p version= < "%~dp0version"
 
 echo ##teamcity[buildNumber '%version%_{build.number}']
 echo ##teamcity[setParameter name='build.version' value='%version%']
-echo ##teamcity[publishArtifacts 'version']
