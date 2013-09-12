48d7ca7f0f38af33b032eb369f428c30a96a30cc
Bastian Eicher <bastian@eicher.net>
2013-09-12 12:59:28 +0200

    Tweaked cleanup script

--- cleanup.cmd
+++ cleanup.cmd
@@ -1,47 +1,48 @@
 @echo off
 ::Removes compilation artifacts and other temporary files.
 
-rem Clear binaries (but leave Documentation intact since it takes very long to build)
-rd /s /q build\Debug > NUL 2>&1
-rd /s /q build\Release > NUL 2>&1
-rd /s /q build\ReleaseSDK > NUL 2>&1
-rd /s /q build\Packages > NUL 2>&1
-rd /s /q build\ProjectTemplates > NUL 2>&1
-rd /s /q build\Setup > NUL 2>&1
-rd /s /q build\Documentation\working > NUL 2>&1
-del build\Documentation\*.log > NUL 2>&1
+rem Clear binaries (but leave Documentation intact since it takes very long to "%~dp0build)
+rd /s /q "%~dp0build\Debug" > NUL 2>&1
+rd /s /q "%~dp0build\Release" > NUL 2>&1
+rd /s /q "%~dp0build\ReleaseSDK" > NUL 2>&1
+rd /s /q "%~dp0build\Packages" > NUL 2>&1
+rd /s /q "%~dp0build\ProjectTemplates" > NUL 2>&1
+rd /s /q "%~dp0build\Setup" > NUL 2>&1
+rd /s /q "%~dp0build\Documentation\working" > NUL 2>&1
+del "%~dp0build\Documentation\*.log" > NUL 2>&1
 
 rem Clear JetBrains caches
-rd /s /q src\_ReSharper.OmegaEngine > NUL 2>&1
-rd /s /q src\_TeamCity.OmegaEngine > NUL 2>&1
+rd /s /q "%~dp0src\_ReSharper.OmegaEngine" > NUL 2>&1
+rd /s /q "%~dp0src\_dotTrace.OmegaEngine" > NUL 2>&1
+rd /s /q "%~dp0src\_TeamCity.OmegaEngine" > NUL 2>&1
 
 rem Clear caches and per-user preferences
-attrib -h src\*.suo > NUL 2>&1
-del src\*.suo > NUL 2>&1
-del src\*.user > NUL 2>&1
-del src\*.cache > NUL 2>&1
-del src\*.ncb > NUL 2>&1
-del src\*.sdf > NUL 2>&1
-rd /s /q src\TerrainSample\Game\obj > NUL 2>&1
-rd /s /q src\TerrainSample\Editor\obj > NUL 2>&1
-rd /s /q src\TerrainSample\Settings\obj > NUL 2>&1
-rd /s /q src\TerrainSample\World\obj > NUL 2>&1
-rd /s /q src\TerrainSample\Test.World\obj > NUL 2>&1
-rd /s /q src\TerrainSample\Presentation\obj > NUL 2>&1
-rd /s /q src\SpaceSample\Game\obj > NUL 2>&1
-rd /s /q src\SpaceSample\Editor\obj > NUL 2>&1
-rd /s /q src\SpaceSample\Settings\obj > NUL 2>&1
-rd /s /q src\SpaceSample\World\obj > NUL 2>&1
-rd /s /q src\SpaceSample\Test.World\obj > NUL 2>&1
-rd /s /q src\SpaceSample\Presentation\obj > NUL 2>&1
-rd /s /q src\Hanoi\obj > NUL 2>&1
-rd /s /q src\Omega\Common\obj > NUL 2>&1
-rd /s /q src\Omega\Test.Common\obj > NUL 2>&1
-rd /s /q src\Omega\Engine\obj > NUL 2>&1
-rd /s /q src\Omega\GUI\obj > NUL 2>&1
-rd /s /q src\Omega\Editor\obj > NUL 2>&1
-rd /s /q src\Modeling\obj > NUL 2>&1
+attrib -h "%~dp0src\*.suo" > NUL 2>&1
+del "%~dp0src\*.suo" > NUL 2>&1
+del "%~dp0src\*.user" > NUL 2>&1
+del "%~dp0src\*.cache" > NUL 2>&1
+del "%~dp0src\*.ncb" > NUL 2>&1
+del "%~dp0src\*.sdf" > NUL 2>&1
+rd /s /q "%~dp0src\TerrainSample\Game\obj" > NUL 2>&1
+rd /s /q "%~dp0src\TerrainSample\Editor\obj" > NUL 2>&1
+rd /s /q "%~dp0src\TerrainSample\Settings\obj" > NUL 2>&1
+rd /s /q "%~dp0src\TerrainSample\World\obj" > NUL 2>&1
+rd /s /q "%~dp0src\TerrainSample\Test.World\obj" > NUL 2>&1
+rd /s /q "%~dp0src\TerrainSample\Presentation\obj" > NUL 2>&1
+rd /s /q "%~dp0src\SpaceSample\Game\obj" > NUL 2>&1
+rd /s /q "%~dp0src\SpaceSample\Editor\obj" > NUL 2>&1
+rd /s /q "%~dp0src\SpaceSample\Settings\obj" > NUL 2>&1
+rd /s /q "%~dp0src\SpaceSample\World\obj" > NUL 2>&1
+rd /s /q "%~dp0src\SpaceSample\Test.World\obj" > NUL 2>&1
+rd /s /q "%~dp0src\SpaceSample\Presentation\obj" > NUL 2>&1
+rd /s /q "%~dp0src\Hanoi\obj" > NUL 2>&1
+rd /s /q "%~dp0src\Omega\Common\obj" > NUL 2>&1
+rd /s /q "%~dp0src\Omega\Test.Common\obj" > NUL 2>&1
+rd /s /q "%~dp0src\Omega\Engine\obj" > NUL 2>&1
+rd /s /q "%~dp0src\Omega\GUI\obj" > NUL 2>&1
+rd /s /q "%~dp0src\Omega\Editor\obj" > NUL 2>&1
+rd /s /q "%~dp0src\Modeling\obj" > NUL 2>&1
 
 rem Remove NUnit logs
-del *.VisualState.xml > NUL 2>&1
-del TestResult.xml > NUL 2>&1
+del "%~dp0*.VisualState.xml" > NUL 2>&1
+del "%~dp0TestResult.xml" > NUL 2>&1
