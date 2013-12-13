dc112ccbe6d6a5940401951e5789751c19bba009
Bastian Eicher <bastian@eicher.net>
2013-12-13 12:35:35 +0100

    Build script: Added support for Visual Studio 2013

--- src/build.cmd
+++ src/build.cmd
@@ -3,6 +3,11 @@
 cd /d "%~dp0"
 
 rem Determine VS version
+if defined VS120COMNTOOLS (
+  ::Visual Studio 2013
+  call "%VS120COMNTOOLS%vsvars32.bat"
+  goto compile
+)
 if defined VS110COMNTOOLS (
   ::Visual Studio 2012
   call "%VS110COMNTOOLS%vsvars32.bat"
