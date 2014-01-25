afd315f8ac95fc299f6a65263e55de2ba59a448f
Bastian Eicher <bastian@eicher.net>
2014-01-25 18:30:52 +0100

    Improved cleanup script

--- cleanup.cmd
+++ cleanup.cmd
@@ -2,39 +2,33 @@
 ::Removes compilation artifacts and other temporary files.
 
 rem Clear binaries
-rd /s /q "%~dp0build"
+rd /s /q "%~dp0build" > NUL 2>&1
+
+rem Clear object caches
+FOR /d %%D IN ("%~dp0src\*") DO (
+  rd /s /q "%%D/obj" > NUL 2>&1
+  del "%%D/*.pidb" > NUL 2>&1
+)
+FOR /d %%D IN ("%~dp0src\AlphaFramework\*") DO (
+  rd /s /q "%%D/obj" > NUL 2>&1
+  del "%%D/*.pidb" > NUL 2>&1
+)
+FOR /d %%D IN ("%~dp0src\TerrainSample\*") DO (
+  rd /s /q "%%D/obj" > NUL 2>&1
+  del "%%D/*.pidb" > NUL 2>&1
+)
+
+rem Clear temporary IDE files
+del "%~dp0src\*.userprefs" > NUL 2>&1
+attrib -h "%~dp0src\*.suo" > NUL 2>&1
+del "%~dp0src\*.suo" > NUL 2>&1
+del "%~dp0src\*.user" > NUL 2>&1
+del "%~dp0src\*.cache" > NUL 2>&1
 
 rem Clear JetBrains caches
 rd /s /q "%~dp0src\_ReSharper.OmegaEngine" > NUL 2>&1
 rd /s /q "%~dp0src\_dotTrace.OmegaEngine" > NUL 2>&1
 rd /s /q "%~dp0src\_TeamCity.OmegaEngine" > NUL 2>&1
-
-rem Clear caches and per-user preferences
-attrib -h "%~dp0src\*.suo" > NUL 2>&1
-del "%~dp0src\*.suo" > NUL 2>&1
-del "%~dp0src\*.user" > NUL 2>&1
-del "%~dp0src\*.cache" > NUL 2>&1
-del "%~dp0src\*.ncb" > NUL 2>&1
-del "%~dp0src\*.sdf" > NUL 2>&1
-rd /s /q "%~dp0src\Common\obj" > NUL 2>&1
-rd /s /q "%~dp0src\Test.Common\obj" > NUL 2>&1
-rd /s /q "%~dp0src\OmegaEngine\obj" > NUL 2>&1
-rd /s /q "%~dp0src\OmegaGUI\obj" > NUL 2>&1
-rd /s /q "%~dp0src\AlphaFramework\Presentation\obj" > NUL 2>&1
-rd /s /q "%~dp0src\AlphaFramework\World\obj" > NUL 2>&1
-rd /s /q "%~dp0src\AlphaFramework\Test.World\obj" > NUL 2>&1
-rd /s /q "%~dp0src\AlphaFramework\Editor\obj" > NUL 2>&1
-rd /s /q "%~dp0src\TerrainSample\Game\obj" > NUL 2>&1
-rd /s /q "%~dp0src\TerrainSample\Editor\obj" > NUL 2>&1
-rd /s /q "%~dp0src\TerrainSample\World\obj" > NUL 2>&1
-rd /s /q "%~dp0src\TerrainSample\Test.World\obj" > NUL 2>&1
-rd /s /q "%~dp0src\TerrainSample\Presentation\obj" > NUL 2>&1
-rd /s /q "%~dp0templates\WinForms\obj" > NUL 2>&1
-rd /s /q "%~dp0templates\WinForms\bin" > NUL 2>&1
-rd /s /q "%~dp0templates\Fullscreen\obj" > NUL 2>&1
-rd /s /q "%~dp0templates\Fullscreen\bin" > NUL 2>&1
-rd /s /q "%~dp0templates\Editor\obj" > NUL 2>&1
-rd /s /q "%~dp0templates\Editor\bin" > NUL 2>&1
 
 rem Remove NUnit logs
 del "%~dp0*.VisualState.xml" > NUL 2>&1
