d86c892d8ef2375c1d22a4034d9b54520174bcb3
Bastian Eicher <bastian@eicher.net>
2014-01-25 18:45:05 +0100

    Improved cleanup script

--- cleanup.cmd
+++ cleanup.cmd
@@ -1,35 +1,38 @@
 @echo off
 ::Removes compilation artifacts and other temporary files.
 
-rem Clear binaries
+rem Compiled artifacts
 rd /s /q "%~dp0build" > NUL 2>&1
 
-rem Clear object caches
-FOR /d %%D IN ("%~dp0src\*") DO (
-  rd /s /q "%%D/obj" > NUL 2>&1
-  del "%%D/*.pidb" > NUL 2>&1
-)
-FOR /d %%D IN ("%~dp0src\AlphaFramework\*") DO (
-  rd /s /q "%%D/obj" > NUL 2>&1
-  del "%%D/*.pidb" > NUL 2>&1
-)
-FOR /d %%D IN ("%~dp0src\TerrainSample\*") DO (
-  rd /s /q "%%D/obj" > NUL 2>&1
-  del "%%D/*.pidb" > NUL 2>&1
-)
-
-rem Clear temporary IDE files
+rem Solution-wide
 del "%~dp0src\*.userprefs" > NUL 2>&1
 attrib -h "%~dp0src\*.suo" > NUL 2>&1
 del "%~dp0src\*.suo" > NUL 2>&1
 del "%~dp0src\*.user" > NUL 2>&1
 del "%~dp0src\*.cache" > NUL 2>&1
 
-rem Clear JetBrains caches
+rem Per-project
+FOR /d %%D IN ("%~dp0src\*") DO (
+  rd /s /q "%%D\obj" > NUL 2>&1
+  del "%%D\*.pidb" > NUL 2>&1
+  del "%%D\*.csproj.user" > NUL 2>&1
+)
+FOR /d %%D IN ("%~dp0src\AlphaFramework\*") DO (
+  rd /s /q "%%D\obj" > NUL 2>&1
+  del "%%D\*.pidb" > NUL 2>&1
+  del "%%D\*.csproj.user" > NUL 2>&1
+)
+FOR /d %%D IN ("%~dp0src\TerrainSample\*") DO (
+  rd /s /q "%%D\obj" > NUL 2>&1
+  del "%%D\*.pidb" > NUL 2>&1
+  del "%%D\*.csproj.user" > NUL 2>&1
+)
+
+rem NUnit logs
+del "%~dp0*.VisualState.xml" > NUL 2>&1
+del "%~dp0TestResult.xml" > NUL 2>&1
+
+rem JetBrains caches
 rd /s /q "%~dp0src\_ReSharper.OmegaEngine" > NUL 2>&1
 rd /s /q "%~dp0src\_dotTrace.OmegaEngine" > NUL 2>&1
 rd /s /q "%~dp0src\_TeamCity.OmegaEngine" > NUL 2>&1
-
-rem Remove NUnit logs
-del "%~dp0*.VisualState.xml" > NUL 2>&1
-del "%~dp0TestResult.xml" > NUL 2>&1
