fa193b8d661215b3f4d6713061af61aa450d623a
Bastian Eicher <bastian@eicher.net>
2014-03-15 11:00:22 +0100

    Fixed assembly signing and InternalsVisibleTo

--- src/AlphaFramework/AssemblyInfo.Global.cs
+++ src/AlphaFramework/AssemblyInfo.Global.cs
@@ -4,13 +4,11 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 
 // Assembly info
-
 [assembly: AssemblyCompany("NanoByte")]
 [assembly: AssemblyProduct("OmegaEngine")]
 [assembly: AssemblyCopyright("Copyright 2006-2014 Bastian Eicher")]
 
 // Version information
-
 [assembly: AssemblyVersion("0.8.4")]
 [assembly: AssemblyFileVersion("0.8.4")]
 
--- src/TerrainSample/AssemblyInfo.Global.cs
+++ src/TerrainSample/AssemblyInfo.Global.cs
@@ -4,13 +4,11 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 
 // Assembly info
-
 [assembly: AssemblyCompany("NanoByte")]
 [assembly: AssemblyProduct("Terrain Sample Game")]
-[assembly: AssemblyCopyright("Copyright ???")]
+[assembly: AssemblyCopyright("Copyright 2006-2014 Bastian Eicher")]
 
 // Version information
-
 [assembly: AssemblyVersion("0.8.4")]
 [assembly: AssemblyFileVersion("0.8.4")]
 
