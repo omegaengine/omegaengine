82e5b7a3fb6d2acfda71e491e744e297d8e7329e
Bastian Eicher <bastian@eicher.net>
2014-05-02 15:06:00 +0200

    Misc tweaks

--- src/OmegaEngine/GlobalSuppressions.cs
+++ src/OmegaEngine/GlobalSuppressions.cs
@@ -8,6 +8,5 @@
 // "In Project Suppression File".
 // You do not need to add suppressions to this file manually.
 
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes", Scope = "member", Target = "OmegaEngine.Assets.Asset.#System.IComparable`1<OmegaEngine.Assets.Asset>.CompareTo(OmegaEngine.Assets.Asset)", Justification = "Comparison only used for INamed sorting")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA2204:Literals should be spelled correctly", MessageId = "Renderables", Scope = "member", Target = "OmegaEngine.DebugForm.#InitializeComponent()")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA2204:Literals should be spelled correctly", MessageId = "OmegaEngine", Scope = "member", Target = "OmegaEngine.DebugForm.#InitializeComponent()")]
