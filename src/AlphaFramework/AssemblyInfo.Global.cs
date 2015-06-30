363fd44b265fe0c2754b4182b1ec3ffd690b65a8
Bastian Eicher <bastian@eicher.net>
2015-06-30 17:34:41 +0200

    Incremented version number

--- src/AlphaFramework/AssemblyInfo.Global.cs
+++ src/AlphaFramework/AssemblyInfo.Global.cs
@@ -9,8 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCopyright("Copyright 2006-2014 Bastian Eicher")]
 
 // Version information
-[assembly: AssemblyVersion("0.9.9")]
-[assembly: AssemblyFileVersion("0.9.9")]
+[assembly: AssemblyVersion("1.0.0")]
 
 // Security settings
 [assembly: InternalsVisibleTo("AlphaFramework.UnitTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100f3cd86e318750437d1a82f10f73d35fd8218bd43139887580f9c17b35181469d5468d390989a85c2f27645b1cf95832cfaa42dd9ea71baefc58191836d46b9c8889caf67213b91d5e383825a61582a59cd197f7f21a66168946cc1558c836620f298ea6d9c8540b6d02fb364bc32a0e33ad7ca78a23a6de83a0f7ccd4e2e88c7")]
--- src/AssemblyInfo.Global.cs
+++ src/AssemblyInfo.Global.cs
@@ -3,12 +3,9 @@
 using System.Reflection;
 
 // Assembly info
-
 [assembly: AssemblyCompany("NanoByte")]
 [assembly: AssemblyProduct("OmegaEngine")]
 [assembly: AssemblyCopyright("Copyright 2006-2014 Bastian Eicher")]
 
 // Version information
-
-[assembly: AssemblyVersion("0.9.9")]
-[assembly: AssemblyFileVersion("0.9.9")]
+[assembly: AssemblyVersion("1.0.0")]
