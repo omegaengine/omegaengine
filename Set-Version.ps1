35c7e209c83d769231b3d6cf682495f846d51f30
Bastian Eicher <bastian@eicher.net>
2016-02-29 22:06:34 +0100

    Tweaked Set-Version script, renamed AssemblyInfo.Global.cs to GlobalAssemblyInfo.cs

--- Set-Version.ps1
+++ Set-Version.ps1
@@ -1,23 +1,21 @@
-﻿#Sets a new version number in all relevant locations
+﻿Param ([Parameter(Mandatory=$True)] [string]$NewVersion)
+#Sets a new version number in all relevant locations
 $ErrorActionPreference = "Stop"
 $ScriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 
-if ($args.Length -lt 1) { Write-Error "Missing argument" }
-$NewVersion = $args[0]
-
 [System.IO.File]::WriteAllText("$ScriptDir\VERSION", $NewVersion)
 
-(Get-Content "$ScriptDir\src\AssemblyInfo.Global.cs" -Encoding UTF8) `
+(Get-Content "$ScriptDir\src\GlobalAssemblyInfo.cs" -Encoding UTF8) `
   -replace 'AssemblyVersion\(".*"\)', ('AssemblyVersion("' + $NewVersion + '")') |
-  Set-Content "$ScriptDir\src\AssemblyInfo.Global.cs" -Encoding UTF8
+  Set-Content "$ScriptDir\src\GlobalAssemblyInfo.cs" -Encoding UTF8
 [System.IO.File]::WriteAllText("$ScriptDir\VERSION", $NewVersion)
-(Get-Content "$ScriptDir\src\AlphaFramework\AssemblyInfo.Global.cs" -Encoding UTF8) `
+(Get-Content "$ScriptDir\src\AlphaFramework\GlobalAssemblyInfo.cs" -Encoding UTF8) `
   -replace 'AssemblyVersion\(".*"\)', ('AssemblyVersion("' + $NewVersion + '")') |
-  Set-Content "$ScriptDir\src\AlphaFramework\AssemblyInfo.Global.cs" -Encoding UTF8
+  Set-Content "$ScriptDir\src\AlphaFramework\GlobalAssemblyInfo.cs" -Encoding UTF8
 [System.IO.File]::WriteAllText("$ScriptDir\VERSION", $NewVersion)
-(Get-Content "$ScriptDir\src\FrameOfReference\AssemblyInfo.Global.cs" -Encoding UTF8) `
+(Get-Content "$ScriptDir\src\FrameOfReference\GlobalAssemblyInfo.cs" -Encoding UTF8) `
   -replace 'AssemblyVersion\(".*"\)', ('AssemblyVersion("' + $NewVersion + '")') |
-  Set-Content "$ScriptDir\src\FrameOfReference\AssemblyInfo.Global.cs" -Encoding UTF8
+  Set-Content "$ScriptDir\src\FrameOfReference\GlobalAssemblyInfo.cs" -Encoding UTF8
 
 #\templates\*\MyTemplate.vstemplate
 (Get-Content "$ScriptDir\templates\vsix\extension.vsixmanifest" -Encoding UTF8) `
