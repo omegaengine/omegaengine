b334b479f9b0435571ca84b2b7aabf91c7f4e485
Bastian Eicher <bastian@eicher.net>
2019-05-08 01:07:52 +0200

    Improved build script

--- src/build.ps1
+++ src/build.ps1
@@ -2,9 +2,10 @@
 pushd $PSScriptRoot
 
 $vsDir = . "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -property installationPath -format value
-$msBuild = "$vsDir\MSBuild\15.0\Bin\amd64\MSBuild.exe"
+$msBuild = if (Test-Path "$vsDir\MSBuild\Current") {"$vsDir\MSBuild\Current\Bin\amd64\MSBuild.exe"} else {"$vsDir\MSBuild\15.0\Bin\amd64\MSBuild.exe"}
 
 nuget restore
 . $msBuild -v:Quiet -t:Build -p:Configuration=Release
+if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
 
 popd
