92c5b3c31c9d28843f1d6b94df14ec0eae3d1071
Bastian Eicher <bastian@eicher.net>
2019-06-01 15:09:05 +0200

    Added .nojekyll file to auto-generated API docs

--- doc/build.ps1
+++ doc/build.ps1
@@ -1,8 +1,9 @@
 ﻿$ErrorActionPreference = "Stop"
 pushd $PSScriptRoot
 
-# Ensure 0install is in the PATH
+# Ensure 0install is in PATH
 if (!(Get-Command 0install -ErrorAction SilentlyContinue)) {
+    echo "Downloading 0install"
     mkdir -Force "$env:TEMP\zero-install" | Out-Null
     Invoke-WebRequest "https://0install.de/files/0install.exe" -OutFile "$env:TEMP\zero-install\0install.exe"
     $env:PATH = "$env:TEMP\zero-install;$env:PATH"
@@ -18,6 +19,6 @@ Invoke-WebRequest https://common.nano-byte.net/nanobyte-common.tag -OutFile nano
 0install run --batch http://repo.roscidus.com/devel/doxygen AlphaFramework.Doxyfile
 0install run --batch http://repo.roscidus.com/devel/doxygen FrameOfReference.Doxyfile
 
-cp CNAME index.html ..\artifacts\Documentation\
+cp index.html,.nojekyll,CNAME ..\artifacts\Documentation\
 
 popd
