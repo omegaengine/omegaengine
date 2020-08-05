4cc1bd86f4b3079ba36a20ab6aee582564070851
Bastian Eicher <bastian@eicher.net>
2020-08-05 16:17:31 +0200

    Tweaked documentation build scripts

--- doc/build.ps1
+++ doc/build.ps1
@@ -1,24 +1,21 @@
 ﻿$ErrorActionPreference = "Stop"
 pushd $PSScriptRoot
 
-# Ensure 0install is in PATH
-if (!(Get-Command 0install -ErrorAction SilentlyContinue)) {
-    echo "Downloading 0install"
-    mkdir -Force "$env:TEMP\zero-install" | Out-Null
-    Invoke-WebRequest "https://0install.de/files/0install.exe" -OutFile "$env:TEMP\zero-install\0install.exe"
-    $env:PATH = "$env:TEMP\zero-install;$env:PATH"
-}
+echo "Downloading references to other documentation..."
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]'Tls11,Tls12'
+Invoke-WebRequest https://common.nano-byte.net/nanobyte-common.tag -OutFile nanobyte-common.tag
 
 if (Test-Path ..\artifacts\Documentation) {rm -Recurse -Force ..\artifacts\Documentation}
 mkdir ..\artifacts\Documentation | Out-Null
 
-# Download tag files for external references
-Invoke-WebRequest https://common.nano-byte.net/nanobyte-common.tag -OutFile nanobyte-common.tag
+function Run-Doxygen($Doxyfile) {
+    .\_0install.ps1 run --batch http://repo.roscidus.com/devel/doxygen $Doxyfile
+    if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
+}
 
-0install run --batch http://repo.roscidus.com/devel/doxygen OmegaEngine.Doxyfile
-0install run --batch http://repo.roscidus.com/devel/doxygen AlphaFramework.Doxyfile
-0install run --batch http://repo.roscidus.com/devel/doxygen FrameOfReference.Doxyfile
-
+Run-Doxygen OmegaEngine.Doxyfile
+Run-Doxygen AlphaFramework.Doxyfile
+Run-Doxygen FrameOfReference.Doxyfile
 cp index.html ..\artifacts\Documentation\
 
 popd
--- doc/build.sh
+++ doc/build.sh
@@ -2,14 +2,14 @@
 set -e
 cd `dirname $0`
 
+echo "Downloading references to other documentation..."
+curl -sS -o nanobyte-common.tag https://common.nano-byte.net/nanobyte-common.tag
+
 rm -rf ../artifacts/Documentation
 mkdir -p ../artifacts/Documentation
 
-# Download tag files for external references
-curl -o nanobyte-common.tag https://common.nano-byte.net/nanobyte-common.tag
-
-0install run http://repo.roscidus.com/devel/doxygen OmegaEngine.Doxyfile
-0install run http://repo.roscidus.com/devel/doxygen AlphaFramework.Doxyfile
-0install run http://repo.roscidus.com/devel/doxygen FrameOfReference.Doxyfile
-
+VERSION=${1:-1.0-dev}
+./_0install.sh run http://repo.roscidus.com/devel/doxygen OmegaEngine.Doxyfile
+./_0install.sh run http://repo.roscidus.com/devel/doxygen AlphaFramework.Doxyfile
+./_0install.sh run http://repo.roscidus.com/devel/doxygen FrameOfReference.Doxyfile
 cp index.html ../artifacts/Documentation/
