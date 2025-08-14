d472e5f2dc39f8538980d583cdd1a137dbe914d7
Bastian Eicher <bastian@eicher.net>
2025-08-14 19:04:44 +0200

    Generate publish directory for "Frame of Reference"

--- src/build.ps1
+++ src/build.ps1
@@ -7,4 +7,10 @@ if ($env:CI) { $ci = "/p:ContinuousIntegrationBuild=True" }
 dotnet msbuild /v:Quiet /t:Restore /t:Build /p:Configuration=Release /p:Version=$Version $ci
 if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
 
+echo "Prepare Frame of Reference for publishing"
+dotnet msbuild /v:Quiet /t:Publish /p:NoBuild=True /p:BuildProjectReferences=False /p:Configuration=Release /p:TargetFramework=net472 /p:Version=$Version FrameOfReference\Game
+if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
+dotnet msbuild /v:Quiet /t:Publish /p:NoBuild=True /p:BuildProjectReferences=False /p:Configuration=Release /p:TargetFramework=net472 /p:Version=$Version FrameOfReference\Editor
+if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
+
 popd
