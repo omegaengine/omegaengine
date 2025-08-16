abbed667defec36be6ac2d043074e206ab1c2f4c
Bastian Eicher <bastian@eicher.net>
2025-08-16 12:00:07 +0200

    Build 0install feeds for "Frame of Reference"

---
+++ feeds/build.ps1
@@ -0,0 +1,8 @@
+﻿Param ($Version = "1.0.0-pre")
+$ErrorActionPreference = "Stop"
+pushd $PSScriptRoot
+
+.\0install.ps1 run --batch https://apps.0install.net/0install/0template.xml frame-of-reference.xml.template version=$Version
+.\0install.ps1 run --batch https://apps.0install.net/0install/0template.xml frame-of-reference-content.xml.template version=$Version
+
+popd
