98d90fbdf5fb5f00443d45441bd76fd8284b5f02
Bastian Eicher <bastian@eicher.net>
2025-12-20 14:25:15 +0100

    Added FirstPersonCamera

---
+++ src/OmegaEngine/Graphics/Cameras/FirstPersonCamera.cs
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2006-2014 Bastian Eicher
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+using System;
+using System.ComponentModel;
+using NanoByte.Common;
+using OmegaEngine.Foundation.Geometry;
+using SlimDX;
+using Resources = OmegaEngine.Properties.Resources;
+
+namespace OmegaEngine.Graphics.Cameras;
+
+/// <summary>
+/// A first-person camera that simulates human-like movement with a fixed "up" direction.
+/// </summary>
+public sealed class FirstPersonCamera : QuaternionCamera
+{
+    private double _yaw;
+
+    /// <summary>
+    /// The clockwise horizontal rotation in degrees.
+    /// </summary>
+    [Description("The clockwise horizontal rotation in degrees."), Category("Layout")]
+    public double Yaw
+    {
+        get => _yaw.RadianToDegree();
+        set
+        {
+            #region Sanity checks
+            if (double.IsInfinity(value) || double.IsNaN(value)) throw new ArgumentOutOfRangeException(nameof(value), Resources.NumberNotReal);
+            #endregion
+
+            value.DegreeToRadian()
+                 .Modulo(2 * Math.PI)
+                 .To(ref _yaw, ref ViewDirty, ref ViewFrustumDirty);
+        }
+    }
+
+    private double _pitch;
+
+    /// <summary>
+    /// The clockwise vertical rotation in degrees.
+    /// </summary>
+    [Description("The clockwise vertical rotation in degrees."), Category("Layout")]
+    public double Pitch
+    {
+        get => _pitch.RadianToDegree();
+        set
+        {
+            #region Sanity checks
+            if (double.IsInfinity(value) || double.IsNaN(value)) throw new ArgumentOutOfRangeException(nameof(value), Resources.NumberNotReal);
+            #endregion
+
+            value.DegreeToRadian()
+                 .Clamp(-Math.PI / 2, Math.PI / 2)
+                 .To(ref _pitch, ref ViewDirty, ref ViewFrustumDirty);
+        }
+    }
+
+    private static readonly DoubleVector3 _defaultWorldUp = new(0, 1, 0);
+    private DoubleVector3 _worldUp = _defaultWorldUp;
+
+    /// <summary>
+    /// A unit vector describing the direction considered up in the world.
+    /// </summary>
+    [Description("A unit vector describing the direction considered up in the world."), Category("Layout")]
+    public DoubleVector3 WorldUp
+    {
+        get => _worldUp;
+        set => value.Normalize().To(ref _worldUp, ref ViewDirty, ref ViewFrustumDirty);
+    }
+
+    /// <inheritdoc/>
+    public override void Navigate(DoubleVector3 translation, DoubleVector3 rotation)
+    {
+        Position += translation
+                   .RotateAroundAxis(new(0, 1, 0), -_yaw)
+                   .AdjustReference(from: _defaultWorldUp, to: _worldUp);
+
+        Yaw += rotation.X;
+        Pitch += rotation.Y;
+    }
+
+    /// <inheritdoc/>
+    protected override void UpdateView()
+    {
+        if (!ViewDirty) return;
+
+        (var axis, double rotation) = _defaultWorldUp.GetRotationTo(_worldUp);
+        Quaternion = Quaternion.RotationAxis((Vector3)axis, (float)-rotation)
+                   * Quaternion.RotationYawPitchRoll((float)_yaw, pitch: 0, roll: 0)
+                   * Quaternion.RotationYawPitchRoll(yaw: 0, (float)_pitch, roll: 0);
+
+        base.UpdateView();
+    }
+}
