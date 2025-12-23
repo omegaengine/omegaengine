6bd83ddf8f992d30bae75fc37e579eaacaa36f1e
Bastian Eicher <bastian@eicher.net>
2025-12-23 16:54:53 +0100

    Added FreeFlyCamera.Axis and .Angle properties

--- src/OmegaEngine/Graphics/Cameras/FreeFlyCamera.cs
+++ src/OmegaEngine/Graphics/Cameras/FreeFlyCamera.cs
@@ -1,6 +1,11 @@
+using System;
+using System.ComponentModel;
+using System.Drawing.Design;
 using NanoByte.Common;
+using OmegaEngine.Foundation.Design;
 using OmegaEngine.Foundation.Geometry;
 using SlimDX;
+using Resources = OmegaEngine.Properties.Resources;
 
 namespace OmegaEngine.Graphics.Cameras;
 
@@ -9,6 +14,34 @@ namespace OmegaEngine.Graphics.Cameras;
 /// </summary>
 public class FreeFlyCamera : QuaternionCamera
 {
+    /// <summary>
+    /// A unit vector describing the axis around which the camera is rotated.
+    /// </summary>
+    [Description("A unit vector describing the axis around which the camera is rotated."), Category("Layout")]
+    public DoubleVector3 Axis
+    {
+        get => (DoubleVector3)Quaternion.Axis;
+        set => Quaternion = Quaternion.RotationAxis((Vector3)value, Quaternion.Angle);
+    }
+
+    /// <summary>
+    /// The rotation around the <see cref="Axis"/> in degrees.
+    /// </summary>
+    [Description("The rotation around the axis in degrees."), Category("Layout")]
+    [Editor(typeof(AngleEditor), typeof(UITypeEditor))]
+    public float Angle
+    {
+        get => Quaternion.Angle.RadianToDegree();
+        set
+        {
+            #region Sanity checks
+            if (double.IsInfinity(value) || double.IsNaN(value)) throw new ArgumentOutOfRangeException(nameof(value), Resources.NumberNotReal);
+            #endregion
+
+            Quaternion = Quaternion.RotationAxis(Quaternion.Axis, value.DegreeToRadian());
+        }
+    }
+
     /// <inheritdoc/>
     public override void Navigate(DoubleVector3 translation, DoubleVector3 rotation)
     {
