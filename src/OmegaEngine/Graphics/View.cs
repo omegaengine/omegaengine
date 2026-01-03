c45a448d1e3be22b3068bc3594ecdfdb56fcacac
Bastian Eicher <bastian@eicher.net>
2026-01-03 15:31:57 +0100

    Fixed creation of Views with fixed background color

--- src/OmegaEngine/Graphics/View.cs
+++ src/OmegaEngine/Graphics/View.cs
@@ -11,7 +11,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
 using NanoByte.Common;
-using OmegaEngine.Assets;
 using OmegaEngine.Foundation.Geometry;
 using OmegaEngine.Foundation.Light;
 using OmegaEngine.Graphics.Cameras;
@@ -252,9 +251,6 @@ public partial class View : EngineElement, IResetable
     public View(Color color)
         : this(new Scene(), new ArcballCamera())
     {
-        var model = new Model(XMesh.Get(Engine, "Lamp.x"));
-        model.Materials[0].Glow = Color.Yellow;
-
         _backgroundColor = color;
         _disposeScene = true;
     }
