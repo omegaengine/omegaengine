39ca685aee752fcbdb9136992b34df0a6b25d62a
Bastian Eicher <bastian@eicher.net>
2026-01-11 13:59:57 +0100

    Added Vector3Ray

---
+++ src/OmegaEngine.Foundation/Design/Vector3RayConverter.cs
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2006-2014 Bastian Eicher
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Globalization;
+using System.Reflection;
+using NanoByte.Common.Values.Design;
+using OmegaEngine.Foundation.Geometry;
+using SlimDX;
+
+namespace OmegaEngine.Foundation.Design;
+
+internal class Vector3RayConverter : ValueTypeConverter<Vector3Ray>
+{
+    /// <inheritdoc/>
+    protected override int NoArguments => 4;
+
+    /// <inheritdoc/>
+    protected override ConstructorInfo GetConstructor() => typeof(Vector3Ray).GetConstructor([
+        typeof(float),
+        typeof(float),
+        typeof(float),
+        typeof(float),
+        typeof(float),
+        typeof(float)
+    ])!;
+
+    /// <inheritdoc/>
+    protected override object[] GetArguments(Vector3Ray value) =>
+    [
+        value.Origin.X,
+        value.Origin.Y,
+        value.Origin.Z,
+        value.Direction.X,
+        value.Direction.Y,
+        value.Direction.Z
+    ];
+
+    /// <inheritdoc/>
+    protected override string[] GetValues(Vector3Ray value, ITypeDescriptorContext? context, CultureInfo culture)
+    {
+        var floatConverter = TypeDescriptor.GetConverter(typeof(float));
+        return
+        [
+            floatConverter.ConvertToString(context, culture, value.Origin.X) ?? "",
+            floatConverter.ConvertToString(context, culture, value.Origin.Y) ?? "",
+            floatConverter.ConvertToString(context, culture, value.Origin.Z) ?? "",
+            floatConverter.ConvertToString(context, culture, value.Direction.X) ?? "",
+            floatConverter.ConvertToString(context, culture, value.Direction.Y) ?? "",
+            floatConverter.ConvertToString(context, culture, value.Direction.Z) ?? ""
+        ];
+    }
+
+    /// <inheritdoc/>
+    protected override Vector3Ray GetObject(string[] values, CultureInfo culture)
+    {
+        #region Sanity checks
+        if (values == null) throw new ArgumentNullException(nameof(values));
+        if (culture == null) throw new ArgumentNullException(nameof(culture));
+        #endregion
+
+        return new(
+            new(Convert.ToSingle(values[0], culture), Convert.ToSingle(values[1], culture), Convert.ToSingle(values[2], culture)),
+            new(Convert.ToSingle(values[3], culture), Convert.ToSingle(values[4], culture), Convert.ToSingle(values[5], culture)));
+    }
+
+    /// <inheritdoc/>
+    protected override Vector3Ray GetObject(IDictionary propertyValues)
+    {
+        #region Sanity checks
+        if (propertyValues == null) throw new ArgumentNullException(nameof(propertyValues));
+        #endregion
+
+        return new((Vector3)propertyValues["Point"]!, (Vector3)propertyValues["Direction"]!);
+    }
+}
---
+++ src/OmegaEngine.Foundation/Geometry/Vector3Ray.cs
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2006-2014 Bastian Eicher
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+using System;
+using System.ComponentModel;
+using System.Globalization;
+using OmegaEngine.Foundation.Design;
+using SlimDX;
+
+namespace OmegaEngine.Foundation.Geometry;
+
+/// <summary>
+/// Defines a ray in three dimensions, specified by an origin and a direction.
+/// </summary>
+/// <param name="origin">A point along the ray.</param>
+/// <param name="direction">A unit vector specifying the direction in which the ray is pointing (automatically normalized).</param>
+[TypeConverter(typeof(Vector3RayConverter))]
+public struct Vector3Ray(Vector3 origin, Vector3 direction) : IEquatable<Vector3Ray>
+{
+    /// <summary>
+    /// A point along the ray.
+    /// </summary>
+    [Description("A point along the ray.")]
+    public Vector3 Origin { get; } = origin;
+
+    private Vector3 _direction = Vector3.Normalize(direction);
+
+    /// <summary>
+    /// A unit vector specifying the direction in which the ray is pointing (automatically normalized).
+    /// </summary>
+    [Description("A unit vector specifying the direction in which the ray is pointing (automatically normalized).")]
+    public Vector3 Direction { readonly get => _direction; set => _direction = Vector3.Normalize(value); }
+
+    /// <inheritdoc/>
+    public readonly override string ToString() => string.Format(CultureInfo.InvariantCulture, "({0} => {1})", Origin, Direction);
+
+    /// <inheritdoc/>
+    public bool Equals(Vector3Ray other) => other.Direction == Direction && other.Origin == Origin;
+
+    public static bool operator ==(Vector3Ray left, Vector3Ray right) => left.Equals(right);
+    public static bool operator !=(Vector3Ray left, Vector3Ray right) => !left.Equals(right);
+
+    /// <summary>
+    /// Computes the perpendicular distance of this ray to a point.
+    /// </summary>
+    public float PerpendicularDistance(Vector3 point)
+    {
+        var toPoint = point - Origin;
+        float projection = Vector3.Dot(toPoint, Direction);
+        var projectedPoint = Origin + Direction * projection;
+        return (point - projectedPoint).Length();
+    }
+
+    /// <inheritdoc/>
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        return obj is Vector3Ray ray && Equals(ray);
+    }
+
+    /// <inheritdoc/>
+    public readonly override int GetHashCode() => HashCode.Combine(Direction, Origin);
+}
