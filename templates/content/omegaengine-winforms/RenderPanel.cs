8f23237dec1127131989bdb7bffd10ea0ca57c84
Bastian Eicher <bastian@eicher.net>
2026-01-13 16:07:42 +0100

    Removed unused RenderPanel shim from template

--- templates/content/omegaengine-winforms/RenderPanel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Template.WinForms;
-
-/// <summary>
-/// Shim to make <see cref="OmegaEngine.RenderPanel"/> accessible via the Visual Studio toolbox.
-/// </summary>
-internal class RenderPanel : OmegaEngine.RenderPanel
-{}
