2b2bcde034bff87fb63c758f32002b1cf993ac97
Bastian Eicher <bastian@eicher.net>
2026-02-03 23:25:55 +0100

    Move 0template output to local 0repo instance

--- feeds/build.ps1
+++ feeds/build.ps1
@@ -5,4 +5,10 @@ pushd $PSScriptRoot
 .\0install.ps1 run --batch https://apps.0install.net/0install/0template.xml frame-of-reference.xml.template version=$Version
 .\0install.ps1 run --batch https://apps.0install.net/0install/0template.xml frame-of-reference-content.xml.template version=$Version
 
+# Move output to local 0repo instance, if present
+if (Test-Path ..\..\publishing\incoming) {
+    mv *.xml ..\..\publishing\incoming\
+    mv *.tar.zst ..\..\publishing\incoming\
+}
+
 popd
