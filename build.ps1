be4280caea54fff45d83a011cf291a078f44320f
Bastian Eicher <bastian@eicher.net>
2026-05-08 23:58:12 +0200

    Enabled independent releases of "Frame of Reference" and "Frame of Reference: Content" feeds

--- build.ps1
+++ build.ps1
@@ -1,4 +1,11 @@
-﻿Param ($Version = "1.0.0-pre")
+﻿<#
+.PARAMETER Version
+    Version number for OmegaEngine and Frame of Reference.
+.PARAMETER ContentVersion
+    Version number of an already existing release of the Frame of Reference content feed to use.
+    Leave unset to build a new release with the same version as the game.
+#>
+Param ($Version = "1.0.0-pre", [string]$ContentVersion = $null)
 $ErrorActionPreference = "Stop"
 pushd $PSScriptRoot
 
@@ -6,6 +13,6 @@ src\build.ps1 $Version
 src\test.ps1
 templates\build.ps1 $Version
 doc\build.ps1
-feeds\build.ps1 $Version
+feeds\build.ps1 $Version $ContentVersion
 
 popd
--- feeds/build.ps1
+++ feeds/build.ps1
@@ -1,9 +1,19 @@
-﻿Param ($Version = "1.0.0-pre")
+﻿<#
+.PARAMETER Version
+    Version number for OmegaEngine and Frame of Reference.
+.PARAMETER ContentVersion
+    Version number of an already existing release of the Frame of Reference content feed to use.
+    Leave unset to build a new release with the same version as the game.
+#>
+Param ($Version = "1.0.0-pre", [string]$ContentVersion = $null)
 $ErrorActionPreference = "Stop"
 pushd $PSScriptRoot
 
-.\0template.ps1 frame-of-reference.xml.template version=$Version
-.\0template.ps1 frame-of-reference-content.xml.template version=$Version
+.\0template.ps1 frame-of-reference.xml.template version=$Version content-version=$(if ($ContentVersion) { $ContentVersion } else { $Version })
+
+if (-not $ContentVersion) {
+    .\0template.ps1 frame-of-reference-content.xml.template version=$Version
+}
 
 # Move output to local 0repo instance, if present
 if (Test-Path ..\..\publishing\incoming) {
