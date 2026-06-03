211a56b0dc7b62a92f64f99524cb9bb2533e9679
Bastian Eicher <bastian@eicher.net>
2026-06-04 00:18:06 +0200

    Use primary constructor

--- src/OmegaEngine/EngineEffects.cs
+++ src/OmegaEngine/EngineEffects.cs
@@ -6,7 +6,6 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-using System;
 using OmegaEngine.Foundation.Light;
 
 namespace OmegaEngine;
@@ -14,38 +13,28 @@ namespace OmegaEngine;
 /// <summary>
 /// Turn specific rendering effects in the <see cref="Engine"/> on or off.
 /// </summary>
-public sealed class EngineEffects
+/// <param name="capabilities">Determines which effects can be turned on.</param>
+public sealed class EngineEffects(EngineCapabilities capabilities)
 {
-    private readonly EngineCapabilities _capabilities;
-
-    /// <summary>
-    /// Creates a new engine effects object.
-    /// </summary>
-    /// <param name="capabilities">Determines which effects can be turned on.</param>
-    internal EngineEffects(EngineCapabilities capabilities)
-    {
-        _capabilities = capabilities ?? throw new ArgumentNullException(nameof(capabilities));
-    }
-
     private bool _perPixelLighting, _normalMapping, _postScreenEffects;
 
     /// <summary>
     /// Use per-pixel lighting
     /// </summary>
     /// <seealso cref="EngineCapabilities.PerPixelEffects"/>
-    public bool PerPixelLighting { get => _perPixelLighting; set => _perPixelLighting = _capabilities.PerPixelEffects && value; }
+    public bool PerPixelLighting { get => _perPixelLighting; set => _perPixelLighting = capabilities.PerPixelEffects && value; }
 
     /// <summary>
     /// Use normal mapping
     /// </summary>
     /// <seealso cref="EngineCapabilities.PerPixelEffects"/>
-    public bool NormalMapping { get => _normalMapping; set => _normalMapping = _capabilities.PerPixelEffects && value; }
+    public bool NormalMapping { get => _normalMapping; set => _normalMapping = capabilities.PerPixelEffects && value; }
 
     /// <summary>
     /// Use post-screen effects
     /// </summary>
     /// <seealso cref="EngineCapabilities.PerPixelEffects"/>
-    public bool PostScreenEffects { get => _postScreenEffects; set => _postScreenEffects = _capabilities.PerPixelEffects && value; }
+    public bool PostScreenEffects { get => _postScreenEffects; set => _postScreenEffects = capabilities.PerPixelEffects && value; }
 
     /// <summary>
     /// Enable or disable shadowing casting (does not affect terrain self-shadowing)
@@ -58,7 +47,7 @@ public sealed class EngineEffects
     /// Sample terrain textures twice with different texture coordinates for better image quality
     /// </summary>
     /// <seealso cref="EngineCapabilities.DoubleSampling"/>
-    public bool DoubleSampling { get => _doubleSampling; set => _doubleSampling = _capabilities.DoubleSampling && value; }
+    public bool DoubleSampling { get => _doubleSampling; set => _doubleSampling = capabilities.DoubleSampling && value; }
 
     private WaterEffectsType _waterEffects = WaterEffectsType.None;
 
@@ -70,6 +59,6 @@ public sealed class EngineEffects
         get => _waterEffects;
         set =>
             // Check if the selected effect mode is supported by the hardware
-            _waterEffects = _capabilities.PerPixelEffects ? value : WaterEffectsType.None;
+            _waterEffects = capabilities.PerPixelEffects ? value : WaterEffectsType.None;
     }
 }
